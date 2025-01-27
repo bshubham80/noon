9698450260c11b7e8abb4119f3969d1889fc242b
Shubham <shubham.bansal@novatr.com>
2025-01-27 12:07:58 +0530

    Added tab icons and skia package

--- src/navigation/HomeNavigator.tsx
+++ src/navigation/HomeNavigator.tsx
@@ -1,16 +1,52 @@
+import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Listing } from '../screens/Listing';
 import { Cart } from '../screens/Cart';
 import { GameIntro } from '../screens/GameIntro';
+import { Image } from 'react-native';
 
 export const HomeNavigator = createBottomTabNavigator({
   initialRouteName: 'Listing',
   screens: {
-    Listing,
-    GameIntro,
-    Cart,
+    Listing: {
+      screen: Listing,
+      options: {
+        tabBarIcon: ({ focused, size, color }) => (
+          <Image
+            source={require('../../assets/images/home.png')}
+            style={{ width: size, height: size }}
+            tintColor={focused ? color : '#757575'}
+          />
+        ),
+      },
+    },
+    GameIntro: {
+      screen: GameIntro,
+      options: {
+        tabBarIcon: ({ focused, color, size }) => (
+          <Image
+            source={require('../../assets/images/console.png')}
+            style={{ width: size, height: size }}
+            tintColor={focused ? color : '#757575'}
+          />
+        ),
+      },
+    },
+    Cart: {
+      screen: Cart,
+      options: {
+        tabBarIcon: ({ focused, color, size }) => (
+          <Image
+            source={require('../../assets/images/bag.png')}
+            style={{ width: size, height: size }}
+            tintColor={focused ? color : '#757575'}
+          />
+        ),
+      },
+    },
   },
   screenOptions: {
     headerShown: false,
+    tabBarActiveTintColor: 'tomato',
   },
 });
--- src/screens/GameIntro/index.tsx
+++ src/screens/GameIntro/index.tsx
@@ -1,10 +1,5 @@
 import React, { useCallback } from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-} from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export const GameIntro = () => {
